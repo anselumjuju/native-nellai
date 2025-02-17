2fd75dec9838cd5cc7a74e1c7f5e9d76e496070b
anselumjuju <anselumjuju@gmail.com>
2025-02-17 19:30:43 +0530

    fix: theme provider moved to admin route

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,17 +1,20 @@
 import { AppSidebar } from '@/app/admin/ui/layout/sidebar';
 import { SiteHeader } from '@/app/admin/ui/layout/header';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
+import { ThemeProvider } from '@/components/theme-provider';
 
 export default function AdminLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className='[--header-height:calc(theme(spacing.14))]'>
-      <SidebarProvider className='flex flex-col'>
-        <SiteHeader />
-        <div className='flex flex-1'>
-          <AppSidebar />
-          <SidebarInset className='!min-h-[calc(100svh-var(--header-height))]'>{children}</SidebarInset>
-        </div>
-      </SidebarProvider>
-    </div>
+    <ThemeProvider attribute='class' defaultTheme='system' disableTransitionOnChange>
+      <div className='[--header-height:calc(theme(spacing.14))]'>
+        <SidebarProvider className='flex flex-col'>
+          <SiteHeader />
+          <div className='flex flex-1'>
+            <AppSidebar />
+            <SidebarInset className='!min-h-[calc(100svh-var(--header-height))]'>{children}</SidebarInset>
+          </div>
+        </SidebarProvider>
+      </div>
+    </ThemeProvider>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from 'react-hot-toast';
 
 const geistSans = Geist({
@@ -27,10 +26,8 @@ export default function RootLayout({
   return (
     <html lang='en' suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ThemeProvider attribute='class' disableTransitionOnChange>
-          {children}
-          <Toaster position='bottom-right' />
-        </ThemeProvider>
+        {children}
+        <Toaster position='bottom-right' />
       </body>
     </html>
   );
