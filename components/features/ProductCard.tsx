326461329cd8d6fcbcbc6f077f175bfc84ae5896
anselumjuju <anselumjuju@gmail.com>
2025-03-01 13:39:53 +0530

    fix: productcart and location card bug fixed

--- components/features/ProductCard.tsx
+++ components/features/ProductCard.tsx
@@ -15,13 +15,13 @@ interface ProductCardProps {
 
 const ProductCard = ({ product }: ProductCardProps) => {
   return (
-    <Link href={`/products/${product.slug}`} className='w-full max-w-64 h-full mx-auto flex flex-col items-start justify-between gap-0.5 rounded-lg overflow-hidden'>
-      <div className='h-64 aspect-square rounded-lg overflow-hidden'>
-        <Image src={product.mainImage} alt={product.name} width={400} height={400} className='size-full object-cover rounded-lg hover:scale-110 duration-500 bg-neutral-200' />
+    <Link href={`/products/${product.slug}`} className='w-48 sm:w-64 h-full mx-auto flex flex-col items-start justify-between gap-0.5 rounded-lg overflow-hidden'>
+      <div className='w-full aspect-square rounded-lg overflow-clip flex items-center justify-center bg-neutral-100'>
+        <Image src={product.mainImage} alt={product.name} width={400} height={400} className='w-[80%] aspect-square object-cover hover:scale-110 duration-500' />
       </div>
-      <div className='space-y-1'>
+      <div className='w-full space-y-1'>
         <p className='text-base text-primary'>{product.name}</p>
-        <p className='py-1 text-xs text-muted-foreground line-clamp-1'>{product.caption}</p>
+        <p className='w-full py-1 text-xs text-muted-foreground truncate'>{product.caption}</p>
       </div>
       <div className='py-2 flex items-center justify-between w-full'>
         <p>&#8377; {product.originalPrice}</p>
--- components/layout/Locations.tsx
+++ components/layout/Locations.tsx
@@ -12,10 +12,12 @@ const Locations = async () => {
       <h1 className='text-3xl font-medium'>View Our Locations</h1>
       <div className='w-full flex items-center justify-start gap-12 overflow-x-auto no-scrollbar'>
         {locations.map((location: { _id: string; name: string; image: string; slug: string }) => (
-          <Link href={`/locations/${location.slug}`} key={location.slug} className='min-w-20'>
-            <div className='space-y-2'>
-              <Image src={location.image} alt='product' width={400} height={400} className='size-full h-20 object-cover rounded-full' />
-              <p className='text-base text-muted-foreground text-center'>{location.name}</p>
+          <Link href={`/locations/${location.slug}`} key={location.slug} className='group'>
+            <div className='w-full space-y-2 flex flex-col items-center justify-center'>
+              <div className='w-20 aspect-square overflow-hidden rounded-full'>
+                <Image src={location.image} alt='product' width={400} height={400} className='size-full object-cover group-hover:scale-110 duration-300' />
+              </div>
+              <p className='w-full text-base text-muted-foreground text-center'>{location.name}</p>
             </div>
           </Link>
         ))}
