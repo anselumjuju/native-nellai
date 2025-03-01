ca2efebba7e999d0ada9b78435eb4fec2ebe80d1
anselumjuju <anselumjuju@gmail.com>
2025-03-02 00:53:49 +0530

    feat: add products page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,16 +1,38 @@
-import Link from 'next/link';
+import ProductCard from '@/components/features/ProductCard';
 
-const Products = () => {
+interface IProduct {
+  _id: string;
+  name: string;
+  caption: string;
+  categoryId: string;
+  mainImage: string;
+  slug: string;
+  originalPrice: number;
+  discountPrice: number;
+}
+
+const Products = async () => {
+  const { data: products, success: productSuccess }: { data: any; success: boolean } = await (await fetch(`${process.env.BASE_URL}/api/products`, { cache: 'reload' })).json();
+
+  if (!productSuccess) return null;
+
   return (
-    <div className='size-full h-dvh flex flex-col items-center justify-center gap-5'>
-      <h1>Products</h1>
-      <div className='w-full flex flex-col gap-3 items-center justify-start'>
-        {['product1', 'product2', 'product3'].map((product) => (
-          <Link href={`/products/${product}`} key={product}>
-            {product}
-          </Link>
-        ))}
-      </div>
+    <div className='w-full max-w-screen-xl px-3 md:px-5 py-10 mx-auto flex flex-col items-center justify-center gap-12'>
+      <h1 className='text-3xl font-medium capitalize relative after:content-[""] after:h-0.5 after:absolute after:-bottom-2 after:left-1/2 after:-translate-x-1/2 after:w-4/5 after:bg-orange-400'>
+        Discover Our Exclusive Collection
+      </h1>
+
+      {products.length === 0 ? (
+        <div className='w-full h-[50vh] flex items-center justify-center'>
+          <p>No products found</p>
+        </div>
+      ) : (
+        <div className='w-full h-full grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-6 gap-y-8'>
+          {products.map((product: IProduct) => (
+            <ProductCard key={product.slug} product={product} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
