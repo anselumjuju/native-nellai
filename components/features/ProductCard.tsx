d873f32cac681b85420d456da32e810e9c370f04
anselumjuju <anselumjuju@gmail.com>
2025-03-18 00:09:34 +0530

    fix: product card functionality

--- components/features/ProductCard.tsx
+++ components/features/ProductCard.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import useUserStore from '@/store/userStore';
+import { Heart } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -14,20 +18,32 @@ interface ProductCardProps {
 }
 
 const ProductCard = ({ product }: ProductCardProps) => {
+  const { wishlist, addToWishlist, removeFromWishlist } = useUserStore();
+
   return (
-    <Link href={`/products/${product.slug}`} className='w-48 sm:w-64 h-full mx-auto flex flex-col items-start justify-between gap-0.5 rounded-lg overflow-hidden'>
-      <div className='w-full aspect-square rounded-lg overflow-clip flex items-center justify-center bg-neutral-100'>
-        <Image src={product.mainImage} alt={product.name} width={400} height={400} className='w-[80%] aspect-square object-cover hover:scale-110 duration-500' priority />
+    <div className='w-48 sm:w-64 h-full mx-auto flex flex-col items-start justify-between gap-0.5 rounded-lg overflow-hidden relative group'>
+      <Heart
+        className={`size-6 absolute top-2 right-2 text-red-500 stroke-1 scale-0 group-hover:scale-100 duration-100 ${wishlist.includes(product._id) ? 'fill-red-500' : ''}`}
+        onClick={() => (wishlist.includes(product._id) ? removeFromWishlist(product._id) : addToWishlist(product._id))}
+      />
+      <div className='w-full aspect-square rounded-lg overflow-clip bg-neutral-100'>
+        <Link href={`/products/${product.slug}`} className='w-full h-full flex items-center justify-center'>
+          <Image src={product.mainImage} alt={product.name} width={400} height={400} className='w-[80%] aspect-square object-cover hover:scale-110 duration-500' priority />
+        </Link>
       </div>
       <div className='w-full space-y-1'>
-        <p className='text-base text-primary'>{product.name}</p>
+        <Link href={`/products/${product.slug}`}>
+          <p className='text-base text-primary'>{product.name}</p>
+        </Link>
         <p className='w-full py-1 text-xs text-muted-foreground truncate'>{product.caption}</p>
       </div>
       <div className='py-2 flex items-center justify-between w-full'>
         <p>&#8377; {product.originalPrice}</p>
-        <button className='px-5 py-1.5 bg-orange-400 text-white rounded-lg text-sm'>Buy Now</button>
+        <Link href={`/products/${product.slug}`} className='px-5 py-1.5 bg-orange-400 text-white rounded-lg text-sm'>
+          Buy Now
+        </Link>
       </div>
-    </Link>
+    </div>
   );
 };
 
